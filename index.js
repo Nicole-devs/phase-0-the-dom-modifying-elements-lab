6371328a838f0d7cfd4a39dd8e70fd30fa746c5d
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-04-13 14:47:17 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+var victory = document.createElement('h1');
+victory.id = 'victory';
+victory.innerHTML = 'NICOLE is the champion';
+document.body.appendChild(victory);
 // Write your code here!
+
+// newHeader.nodeName('victory') = 'H1'
+// newHeader.id("victory") = "victory"
+// newHeader.innerHTML("victory") = victory.innerHTML
+
+// const h1 = document.getElementById("victory");
+// h1.innerHTML = 'NICOLE is the champion'
+
+var newHeader = document.createElement('H1');
+document.body.appendChild(victory);
+
+document.getElementById("H1");
+newHeader.innerHTML = 
+'<h1>Nicole!</h1> <p> is the champion/p>'
